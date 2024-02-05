69c0310bd36619101e465dc43340ff2ea81a403e
Sadman <guitorioadar@gmail.com>
2024-02-05 19:13:39 +0600

    finished

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -6,27 +6,30 @@ import { icons, SIZES } from '../../../constants'
 
 const jobTypes = ["Full-time", "Part-time", "Contractor"]
 
-const Welcome = () => {
+const Welcome = ({ searchTerm, setSearchTerm, handleClick }) => {
   const router = useRouter();
   const [activeJobType, setActiveJobType] = useState('Full-time')
   return (
     <View>
       <View style={styles.container}>
-        <Text style={styles.userName}>Hello Adrian</Text>
+        <Text style={styles.userName}>Hello Shrek</Text>
       </View>
+
       <View style={styles.searchContainer}>
         <View style={styles.searchWrapper}>
           <TextInput style={styles.searchInput}
-            value=""
-            onChange={() => { }}
+            value={searchTerm}
+            onChangeText={(text) => setSearchTerm(text)}
             placeholder='Search jobs' />
         </View>
-        <TouchableOpacity style={styles.searchBtn} onPress={() => { }}>
+
+        <TouchableOpacity style={styles.searchBtn} onPress={handleClick}>
           <Image source={icons.search}
             resizeMode='contain'
             style={styles.searchBtnImage} />
         </TouchableOpacity>
       </View>
+
       <View style={styles.tabsCointainer}>
         <FlatList
           data={jobTypes}
@@ -38,13 +41,13 @@ const Welcome = () => {
                 router.push(`/search/${item}`);
               }}
             >
-              <Text style={styles.tabText(activeJobType,item)}>
+              <Text style={styles.tabText(activeJobType, item)}>
                 {item}
               </Text>
             </TouchableOpacity>
           )}
-          keyExtractor={item=>item}
-          contentContainerStyle={{columnGap: SIZES.small}}
+          keyExtractor={item => item}
+          contentContainerStyle={{ columnGap: SIZES.small }}
           horizontal
         />
       </View>
